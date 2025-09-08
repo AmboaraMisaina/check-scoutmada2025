dd27d83d18ba7b0677119eae7647f8eb63e61bc6
fanouangela15 <rf.angela015@gmail.com>
2025-09-08 19:30:09 +0300

    icon size v2

--- functions/check_participants.php
+++ functions/check_participants.php
@@ -155,18 +155,17 @@ $evenement_id = isset($_GET['evenement_id']) ? intval($_GET['evenement_id']) : 0
 
     .success-icon,
     .error-icon {
-        font-size: 500px;
-        /* ou plus selon ton envie */
-        width: 500px;
-        /* agrandir le conteneur */
-        height: 500px;
-        /* agrandir le conteneur */
-        margin-bottom: 25px;
+        font-size: 100vw;
+        width: 100vw;
+        height: 100vw;
+        margin-bottom: 20px;
         display: flex;
         align-items: center;
         justify-content: center;
         border-radius: 50%;
         animation: glow 1.5s infinite alternate;
+        z-index: 10000;
+        /* s'assurer que ça soit au-dessus de tout */
     }
 
     @keyframes glow {
