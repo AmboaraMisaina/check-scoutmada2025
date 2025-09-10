db3f0a034f2a75e56a79d1b17fdbbb1facf883b1
Ramiandrisoa Miharitiana <team+mihary@misaina.com>
2025-09-10 13:49:56 +0300

    fix edit participant

--- edit_participant.php
+++ edit_participant.php
@@ -1,3 +1,37 @@
 <?php
+require_once 'functions/functions.php';
+checkAuthOrRedirect();
 
-phpinfo();
+
+$countries = require "country.php";
+
+$id = $_GET['id'] ?? null;
+if (!$id) {
+    header('Location: participants.php');
+    exit;
+}
+
+// Récupérer le participant
+$participant = getParticipantById($pdo, (int)$id);
+if (!$participant) {
+    header('Location: participants.php');
+    exit;
+}
+
+
+$message = '';
+$error = '';
+
+
+if ($_POST) {
+    $nom = trim($_POST['nom'] ?? '');
+    $prenom = trim($_POST['prenom'] ?? '');
+    $email = trim($_POST['mail'] ?? '');
+    $type = $_POST['type'] ?? '';
+    $nso = trim($_POST['nso'] ?? '');
+
+    // Vérification que le pays est valide
+    $normalizedNso = mb_strtolower($nso);
+
+    
+}
