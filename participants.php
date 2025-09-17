656ffa3dfb7c3d8e164b5579d5c25365405dc831
Amboara <amboara@misaina.com>
2025-09-17 09:29:51 +0300

    fix filter paid

--- participants.php
+++ participants.php
@@ -123,8 +123,8 @@ table {
             <input type="text" name="filter_name" placeholder="Search by name" value="<?= htmlspecialchars($filter_name) ?>" style="padding:0.5rem; border-radius:5px; border:1px solid #ccc; flex:1;">
             <select name="filter_paid" style="padding:0.5rem; border-radius:5px; border:1px solid #ccc;">
                 <option value="">Payment</option>
-                <option value="1" <?= ($to_print === '1') ? 'selected' : '' ?>>Paid</option>
-                <option value="0" <?= ($to_print === '0') ? 'selected' : '' ?>>Not Paid</option>
+                <option value="1" <?= ($filter_paid === '1') ? 'selected' : '' ?>>Paid</option>
+                <option value="0" <?= ($filter_paid === '0') ? 'selected' : '' ?>>Not Paid</option>
             </select>
 
 
