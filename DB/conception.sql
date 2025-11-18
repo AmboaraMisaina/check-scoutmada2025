649c7671a90b71d31871d4c8eebac0efdbde5354
Ramiandrisoa Miharitiana <team+mihary@misaina.com>
2025-11-18 13:18:41 +0300

    Update conception.sql

--- DB/conception.sql
+++ DB/conception.sql
@@ -1,3 +1,21 @@
+-- 🔥 1. Création de la base
+CREATE DATABASE IF NOT EXISTS mahaymg1_checkscoutmada2025
+CHARACTER SET utf8mb4
+COLLATE utf8mb4_unicode_ci;
+
+-- 🔥 2. Création de l'utilisateur (même nom, même mot de passe qu'en production)
+CREATE USER IF NOT EXISTS 'mahaymg1_checkscoutmada2025'@'localhost'
+IDENTIFIED BY 'yfJ4CtmnnexBLYCuLt4Y';
+
+-- 🔥 3. Donner tous les droits sur la base
+GRANT ALL PRIVILEGES ON mahaymg1_checkscoutmada2025.*
+TO 'mahaymg1_checkscoutmada2025'@'localhost';
+
+-- 🔥 4. Appliquer les changements
+FLUSH PRIVILEGES;
+
+
+
 create table roles (
   id INT AUTO_INCREMENT PRIMARY KEY,
   label VARCHAR(50) NOT NULL UNIQUE,
